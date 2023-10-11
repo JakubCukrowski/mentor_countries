3cf4f958af171046bfa81dfe5a2aa0fe2e0d7493
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-10-11 22:18:05 +0200

    Added different utilities hiding approach

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,3 +1,5 @@
+//section id=top
+const topSection = document.querySelector("#top");
 //dropdown menu
 const selectContinent = document.querySelector(".dropdown-menu");
 const dropdownList = document.querySelector(".dropdown-list");
@@ -6,8 +8,8 @@ const selectOptions = dropdownList.querySelectorAll("li");
 const countriesContainer = document.querySelector(".countries-container");
 //searchbar
 const searchbar = document.querySelector(".search-bar");
-//utilities
-const utilities = document.querySelector(".utilities");
+//utilities container to delete
+const utilitiesContainer = document.querySelector(".utilities__container");
 //url api changers for filtering
 let region;
 let endpoint;
@@ -49,31 +51,36 @@ const fetchCountries = async ()=>{
     }).catch((err)=>console.log(err));
 };
 //handle dropdown menu
-selectContinent.addEventListener("click", ()=>{
+const handleSelectContinent = ()=>{
     dropdownList.classList.toggle("blocked");
-});
-selectOptions.forEach((option)=>option.addEventListener("click", (event)=>{
-        selectContinent.innerText = event.target.innerText;
-        region = event.target.getAttribute("value");
-        dropdownList.classList.remove("blocked");
-        countriesContainer.innerHTML = "";
-        url = `https://restcountries.com/v3.1/region/${region.toLowerCase()}`;
-        fetchCountries();
-    }));
-//handle searchbar
-searchbar.addEventListener("input", ()=>{
-    endpoint = searchbar.value;
-    if (endpoint.length > 0) url = `https://restcountries.com/v3.1/name/${endpoint}`;
+};
+selectContinent.addEventListener("click", handleSelectContinent);
+const handleOptions = (event)=>{
+    selectContinent.innerText = event.target.innerText;
+    region = event.target.getAttribute("value");
+    dropdownList.classList.remove("blocked");
     countriesContainer.innerHTML = "";
+    url = `https://restcountries.com/v3.1/region/${region.toLowerCase()}`;
     fetchCountries();
-    if (searchbar.value.length <= 0 && region !== undefined) {
+};
+selectOptions.forEach((option)=>option.addEventListener("click", handleOptions));
+//handle searchbar
+const handleSearchbar = (e)=>{
+    endpoint = e.target.value;
+    if (endpoint.length > 0) {
+        url = `https://restcountries.com/v3.1/name/${endpoint}`;
+        countriesContainer.innerHTML = "";
+        fetchCountries();
+    }
+    if (e.target.value.length <= 0 && region !== undefined) {
         url = `https://restcountries.com/v3.1/region/${region.toLowerCase()}`;
         fetchCountries();
-    } else if (searchbar.value.length <= 0 && region === undefined) {
+    } else if (e.target.value.length <= 0 && region === undefined) {
         url = "https://restcountries.com/v3.1/all";
         fetchCountries();
     }
-});
+};
+searchbar.addEventListener("input", handleSearchbar);
 //single country structure 
 const singleCountryStructure = (src, alt, singleCountryName, nativeName, population, countryRegion, subregion, capital, topLevelDomain, currencies, languages)=>{
     const newDiv = document.createElement("div");
@@ -84,7 +91,7 @@ const singleCountryStructure = (src, alt, singleCountryName, nativeName, populat
     newButton.addEventListener("click", ()=>{
         region === undefined ? url = "https://restcountries.com/v3.1/all" : url = `https://restcountries.com/v3.1/region/${region.toLowerCase()}`;
         countriesContainer.innerHTML = "";
-        utilities.style.visibility = "visible";
+        createUtilitiesContainer();
         history.back();
         fetchCountries();
     });
@@ -135,8 +142,9 @@ const singleCountryStructure = (src, alt, singleCountryName, nativeName, populat
 // country details page
 const countryDetail = async (e)=>{
     e.preventDefault();
+    console.log("wykonano");
     countriesContainer.innerHTML = "";
-    utilities.style.visibility = "hidden";
+    utilitiesContainer.remove();
     countryName = e.target.closest("a").id;
     url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     history.pushState(null, null, `/country/${countryName}`);
@@ -144,10 +152,56 @@ const countryDetail = async (e)=>{
         singleCountryStructure(response[0].flags.png, response[0].flag, response[0].name.common, Object.values(response[0].name.nativeName).length > 2 ? Object.values(response[0].name.nativeName)[2].common : Object.values(response[0].name.nativeName)[0].common, response[0].population, response[0].region, response[0].subregion, response[0].capital, response[0].tld[0], Object.values(response[0].currencies)[0].name, Object.values(response[0].languages).join(", "));
     });
 };
+//create utilities container if deleted
+const createUtilitiesContainer = ()=>{
+    const newUtilitiesContainer = document.createElement("div");
+    newUtilitiesContainer.classList.add("container", "utilities__container");
+    topSection.append(newUtilitiesContainer);
+    const utilitiesDiv = document.createElement("div");
+    utilitiesDiv.classList.add("utilities");
+    newUtilitiesContainer.append(utilitiesDiv);
+    const searchbarWrapper = document.createElement("span");
+    searchbarWrapper.classList.add("search-bar-wrapper");
+    utilitiesDiv.append(searchbarWrapper);
+    const searchbarInput = document.createElement("input");
+    searchbarInput.setAttribute("type", "text");
+    searchbarInput.setAttribute("placeholder", "Search for a country...");
+    searchbarInput.classList.add("search-bar");
+    searchbarInput.addEventListener("input", handleSearchbar);
+    searchbarWrapper.append(searchbarInput);
+    const dropdownDiv = document.createElement("div");
+    dropdownDiv.classList.add("dropdown");
+    newUtilitiesContainer.append(dropdownDiv);
+    const newDropdownButton = document.createElement("button");
+    newDropdownButton.classList.add("dropdown-menu");
+    newDropdownButton.innerText = "Filter by Region";
+    newDropdownButton.addEventListener("click", handleSelectContinent);
+    dropdownDiv.append(newDropdownButton);
+    const newDropdownList = document.createElement("ul");
+    newDropdownList.classList.add("dropdown-list");
+    dropdownDiv.append(dropdownList);
+    const newLiOption1 = document.createElement("li");
+    const newLiOption2 = document.createElement("li");
+    const newLiOption3 = document.createElement("li");
+    const newLiOption4 = document.createElement("li");
+    const newLiOption5 = document.createElement("li");
+    newLiOption1.setAttribute("value", "Africa");
+    newLiOption2.setAttribute("value", "Americas");
+    newLiOption3.setAttribute("value", "Asia");
+    newLiOption4.setAttribute("value", "Europe");
+    newLiOption5.setAttribute("value", "Oceania");
+    newLiOption1.addEventListener("click", handleOptions);
+    newLiOption2.addEventListener("click", handleOptions);
+    newLiOption3.addEventListener("click", handleOptions);
+    newLiOption4.addEventListener("click", handleOptions);
+    newLiOption5.addEventListener("click", handleOptions);
+    newDropdownList.append(newLiOption1, newLiOption2, newLiOption3, newLiOption4, newLiOption5);
+};
 //on page load display all, after refresh depends on the URL
 if (window.location.pathname === "/") fetchCountries();
 else {
     url = `https://restcountries.com/v3.1/name/${currentCountry}?fullText=true`;
+    utilitiesContainer.remove();
     fetch(url).then((response)=>response.json()).then((response)=>{
         singleCountryStructure(response[0].flags.png, response[0].flag, response[0].name.common, Object.values(response[0].name.nativeName).length > 2 ? Object.values(response[0].name.nativeName)[2].common : Object.values(response[0].name.nativeName)[0].common, response[0].population, response[0].region, response[0].subregion, response[0].capital, response[0].tld[0], Object.values(response[0].currencies)[0].name, Object.values(response[0].languages).join(", "));
     });
